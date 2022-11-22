df5ca4fc8d6e7131d378d38ae8bc390ae800df86
Sarah Suen <sarahsuen@Sarahs-MacBook-Air.local>
2022-11-21 17:04:43 -0800

    finished hw10

---
+++ final/assets/script.js
@@ -0,0 +1,20 @@
+var navbar = document.querySelector('header')
+
+window.onscroll = function() {
+
+  // pageYOffset or scrollY
+  if (window.pageYOffset > 100) {
+    navbar.classList.add('scrolled')
+  } else {
+    navbar.classList.remove('scrolled')
+  }
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
