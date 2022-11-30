5d3622c3b1476a00a8a952b0a80d134a55407062
Sarah Suen <sarahsuen@Sarahs-MacBook-Air.local>
2022-11-29 22:50:23 -0800

    website first draft

--- final/assets/script.js
+++ final/assets/script.js
@@ -1,12 +1,11 @@
-var navbar = document.querySelector('header')
+var navbar = document.querySelector('header');
 
 window.onscroll = function() {
-
   // pageYOffset or scrollY
   if (window.pageYOffset > 100) {
-    navbar.classList.add('scrolled')
+    navbar.classList.add('scrolled');
   } else {
-    navbar.classList.remove('scrolled')
+    navbar.classList.remove('scrolled');
   }
 }
 
