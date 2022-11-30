d60a9b09fc1467757073b0a4a1f53083569ae020
Sarah Suen <sarahsuen@Sarahs-MacBook-Air.local>
2022-11-30 00:02:41 -0800

    embellishment adjustments

--- final/assets/script.js
+++ final/assets/script.js
@@ -9,11 +9,3 @@ window.onscroll = function() {
   }
 }
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-
